827ed3cb2b14ddeaf683385eeabe99e7de4b4237
MauriAndresMU1313 <mauricio.amm.bio@gmail.com>
2026-06-20 19:47:22 -0400

    First commit

---
+++ src/code_base.R
@@ -0,0 +1,10 @@
+# Helper functions that can be use in all the project
+
+.tiff_pdf_plot <- function() {
+    #' Generate TIFF and PDF plots
+    #' 
+    #' @param ggplot2 object
+    #' @param height
+    #' @param width
+    #' @return TIFF and PDF files
+}
