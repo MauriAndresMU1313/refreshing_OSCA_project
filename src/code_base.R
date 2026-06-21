0214774beace88cbcc7bfaab1126a2a988423872
MauriAndresMU1313 <mauricio.amm.bio@gmail.com>
2026-06-20 21:07:56 -0400

    Adding helper function for tiff and pdf generation, as well as saving intermediary outputs

--- src/code_base.R
+++ src/code_base.R
@@ -1,10 +1,31 @@
 # Helper functions that can be use in all the project
 
-.tiff_pdf_plot <- function() {
+.tiff_pdf_plot <- function(
+    PLOT,
+    HEIGHT,
+    WIDTH,
+    DIRECTORY,
+    FILENAME) {
     #' Generate TIFF and PDF plots
-    #' 
-    #' @param ggplot2 object
-    #' @param height
-    #' @param width
-    #' @return TIFF and PDF files
+    #'
+    #' @param PLOT ggplot2 object to save
+    #' @param HEIGHT Plot height in inches
+    #' @param WIDTH Plot width in inches
+    #' @param DIRECTORY Path to the output directory
+    #' @return TIFF and PDF files written to disk
+
+    ggplot2::ggsave(
+        filename = file.path(DIRECTORY, paste0(FILENAME, ".tiff")),
+        plot = PLOT,
+        height = HEIGHT,
+        width = WIDTH,
+        dpi = 300
+    )
+
+    ggplot2::ggsave(
+        filename = file.path(DIRECTORY, paste0(FILENAME, ".pdf")),
+        plot = PLOT,
+        height = HEIGHT,
+        width = WIDTH
+    )
 }
