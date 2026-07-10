c2c39a05ed5fee71cb020fc1c1aefc6ebb70904d
MauriAndresMU1313 <mauricio.amm.bio@gmail.com>
2026-07-09 20:33:51 -0400

    Adding helper function for Ridge Plot

--- src/code_base.R
+++ src/code_base.R
@@ -29,3 +29,43 @@
         width = WIDTH
     )
 }
+
+.ridge_plot <- function(
+    SEURAT_OBJECT,
+    MARKERS = NULL,
+    PALETTE = NULL,
+    COLUMNS = NULL,
+    ASSAY = "RNA") {
+    #' Recreate Ridge Plot from Seurat by integrating MetBrewer
+    #'
+    #' @param SEURAT_OBJECT Seurat object post QC, processing and marker identification
+    #' @param MARKERS character vector of gene markers for expression level plotting
+    #' @param PALETTE MetBrewer palette name
+    #' @param COLUMNS Number of columns for the layout of plots
+    #' @param ASSAY Assay to use, default `RNA` but `SCT` available as well
+    #' @return patchwork grid of ridge plots
+
+    N_CLUSTERS <- length(levels(Seurat::Idents(SEURAT_OBJECT)))
+    PALETTE_MAX <- length(MetBrewer::met.brewer(PALETTE))
+
+    COLORS <- setNames(
+        if (N_CLUSTERS > PALETTE_MAX) {
+            MetBrewer::met.brewer(PALETTE, n = N_CLUSTERS, type = "continuous")
+        } else {
+            MetBrewer::met.brewer(PALETTE, n = N_CLUSTERS, type = "discrete")
+        },
+        levels(Seurat::Idents(SEURAT_OBJECT))
+    )
+
+    purrr::map(MARKERS, ~ SCpubr::do_RidgePlot(
+        sample = SEURAT_OBJECT,
+        feature = .x,
+        assay = ASSAY,
+        colors.use = COLORS,
+        legend.position = "bottom"
+    )) %>%
+        patchwork::wrap_plots(ncol = COLUMNS) +
+        patchwork::plot_layout(guides = "collect") &
+        ggplot2::theme(legend.position = "bottom")
+}
+
