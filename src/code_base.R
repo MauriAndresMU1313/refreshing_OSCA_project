fbb164a6fe0150ec7c1858086c58ce500ad4b3b2
MauriAndresMU1313 <mauricio.amm.bio@gmail.com>
2026-07-25 19:24:31 -0400

    Adapting violin and feature helper function for better functionality with group by and/or split by options

--- src/code_base.R
+++ src/code_base.R
@@ -141,23 +141,43 @@
 .feature_plot_markers <- function(
     SEURAT_OBJECT,
     MARKERS = NULL,
-    PALATTE = NULL,
-    COLORS = NULL) {
+    PALETTE = NULL,
+    SPLIT_BY = NULL,
+    MAX_CUTOFF = NULL,
+    REDUCTION = "umap",
+    COLUMNS = NULL) {
     #' Feature plots for marker genes arranged in a grid
     #'
     #' @param SEURAT_OBJECT Seurat object post marker identification
     #' @param MARKERS character vector of marker genes to plot
-    #' @param PALETTE MetBrewer palette name, defaults to "Redon"
+    #' @param PALETTE sequential palette name for continuous color scale
+    #' @param SPLIT_BY metadata column to split plots by condition
+    #' @param MAX_CUTOFF maximum expression cutoff value
+    #' @param REDUCTION dimensional reduction to use, defaults to "umap"
+    #' @param COLUMNS number of columns in patchwork layout
     #' @return patchwork grid of feature plots
 
-    # COLORS <- MetBrewer::met.brewer(PALETTE, type = "continuous")
-
-    purrr::map(MARKERS, ~ SCpubr::do_FeaturePlot(
-        sample = SEURAT_OBJECT,
-        features = .x,
-        sequential.palette = COLORS
-    )) %>%
-        patchwork::wrap_plots()
+    if (is.null(SPLIT_BY)) {
+        purrr::map(MARKERS, ~ SCpubr::do_FeaturePlot(
+            sample = SEURAT_OBJECT,
+            features = .x,
+            reduction = REDUCTION,
+            max.cutoff = MAX_CUTOFF,
+            sequential.palette = PALETTE,
+            ncol = COLUMNS
+        )) %>%
+            patchwork::wrap_plots()
+    } else {
+        SCpubr::do_FeaturePlot(
+            sample = SEURAT_OBJECT,
+            features = MARKERS,
+            split.by = SPLIT_BY,
+            reduction = REDUCTION,
+            max.cutoff = MAX_CUTOFF,
+            sequential.palette = PALETTE,
+            ncol = COLUMNS
+        )
+    }
 }
 
 .vln_plot_markers <- function(
@@ -165,17 +185,32 @@
     MARKERS = NULL,
     PALETTE = NULL,
     COLUMNS = NULL,
-    ASSAY = NULL) {
+    ASSAY = NULL,
+    SPLIT_BY = NULL,
+    GROUP_BY = NULL) {
     #' Violin plots for marker genes arranged in a grid
     #'
-    #' @param SEURAT_OBJECT Seurat object post marker identification
-    #' @param MARKERS character vector of marker genes to plot
+    #' @param SEURAT_OBJECT post marker identification
+    #' @param MARKERS character vector of marker
     #' @param PALETTE MetBrewer palette name
     #' @param COLUMNS Number of columns for the layout of plots
     #' @param ASSAY Assay to use
-    #' @return patchwork grid of violin plots
+    #' @param SPLIT_BY metadata column to split plots by condition
+    #' @param GROUP_BY metadata column to group cells by
+    #' @return patchwork grid
 
-    N_CLUSTERS <- length(levels(Seurat::Idents(SEURAT_OBJECT)))
+    # Handle SPLIT_BY or GROUP_BY
+    CLUSTER_IDS <- if (is.null(SPLIT_BY)) {
+        if (is.null(GROUP_BY)) {
+            levels(Seurat::Idents(SEURAT_OBJECT))
+        } else {
+            unique(SEURAT_OBJECT@meta.data[[GROUP_BY]])
+        }
+    } else {
+        unique(SEURAT_OBJECT@meta.data[[SPLIT_BY]])
+    }
+
+    N_CLUSTERS <- length(CLUSTER_IDS)
     PALETTE_MAX <- length(MetBrewer::met.brewer(PALETTE))
 
     COLORS <- setNames(
@@ -184,16 +219,87 @@
         } else {
             MetBrewer::met.brewer(PALETTE, n = N_CLUSTERS, type = "discrete")
         },
-        levels(Seurat::Idents(SEURAT_OBJECT))
+        CLUSTER_IDS
     )
 
     purrr::map(MARKERS, ~ SCpubr::do_ViolinPlot(
         sample = SEURAT_OBJECT,
         features = .x,
         assay = ASSAY,
-        colors.use = COLORS
+        colors.use = COLORS,
+        split.by = SPLIT_BY,
+        group.by = GROUP_BY,
+        order = is.null(SPLIT_BY),
+        plot_boxplot = is.null(SPLIT_BY)
     )) %>%
         patchwork::wrap_plots(ncol = COLUMNS) +
         patchwork::plot_layout(guides = "collect") &
         ggplot2::theme(legend.position = "bottom")
+}
+
+.dot_plot_cons_markers <- function(
+    SEURAT_OBJECT,
+    MARKERS = NULL,
+    SPLIT_BY = NULL,
+    FLIP = FALSE,
+    DOT_SCALE = NULL,
+    TITLE = NULL) {
+    #' Dot plot for conserved markers across conditions
+    #'
+    #' @param SEURAT_OBJECT Seurat object post marker identification
+    #' @param MARKERS character vector or named list of marker genes
+    #' @param SPLIT_BY metadata column to split plot by condition, defaults to NULL
+    #' @param FLIP logical, flip axes, defaults to FALSE
+    #' @param DOT_SCALE size scaling for dots, defaults to NULL
+    #' @param TITLE plot title
+    #' @return ggplot2 dot plot
+
+    SCpubr::do_DotPlot(
+        sample = SEURAT_OBJECT,
+        features = MARKERS,
+        split.by = SPLIT_BY,
+        flip = FLIP,
+        dot.scale = DOT_SCALE,
+        plot.title = TITLE,
+        axis.text.x.angle = 45,
+        use_viridis = TRUE,
+        font.size = 16
+    )
+}
+
+.cell_scatter_comparison <- function(
+    AGGREGATE_OBJECT,
+    CELL_TYPES = NULL,
+    CONDITION_1 = NULL,
+    CONDITION_2 = NULL,
+    MARKERS = NULL,
+    COLUMNS = NULL,
+    SIZE = NULL) {
+    #' Cell scatter plots comparing two conditions per cell type
+    #'
+    #' @param AGGREGATE_OBJECT Aggregated Seurat object
+    #' @param CELL_TYPES character vector of cell types to compare
+    #' @param CONDITION_1
+    #' @param CONDITION_2
+    #' @param MARKERS character vector of genes to highlight and label
+    #' @param COLUMNS number of columns in patchwork layout
+    #' @return patchwork grid of labeled cell scatter plots
+
+    purrr::map(CELL_TYPES, function(CELL_TYPE) {
+        LABEL_1 <- paste0(CELL_TYPE, "_", CONDITION_1)
+        LABEL_2 <- paste0(CELL_TYPE, "_", CONDITION_2)
+
+        Seurat::CellScatter(
+            AGGREGATE_OBJECT,
+            cell1 = LABEL_1,
+            cell2 = LABEL_2,
+            highlight = MARKERS
+        ) %>%
+            Seurat::LabelPoints(
+                points = MARKERS,
+                repel = TRUE,
+                size = SIZE
+            )
+    }) %>%
+        patchwork::wrap_plots(ncol = COLUMNS)
 }
